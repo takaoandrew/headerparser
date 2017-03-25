ad321558bc51162da0d86b7e113d4dbb22198d7d
Andrew Takao <takaoandrew@gmail.com>
2017-03-25 15:33:59 +0000

    complete project

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  var object = {"ip address": "", "language": "", "software": ""}
+var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+  var script = ""
+  object['ip address'] = ip
+  object.lang = req.headers['accept-language']
+  object.software = req.headers['user-agent']
+  res.send(object)
+//  res.send(script)
 })
 
 app.listen(8080, function () {
